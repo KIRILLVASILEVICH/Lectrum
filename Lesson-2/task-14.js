80e7de7bce72d6593d745447eb6f0dacdef4767e
Kirill <jws-89@mail.ru>
2019-04-08 02:13:22 +0300

    Decided finally

--- Lesson-2/task-14.js
+++ Lesson-2/task-14.js
@@ -1,7 +1,7 @@
-// var num = 0;
-// var n = 1000;
-// for (var i = 0; i < n; i++) {
-//     if (n / 2 && n < 50) {
-//         console.log(n);
-//     }
-// }
+var n = 1000;
+var i = 0;
+for (var i = 0; n > 50; i++) {
+    n = n / 2;
+}
+console.log(n);
+console.log(i);
